b15437aef0c84c697ad639c7e596390774255dcf
Gura-Andriy <gura3a@gmail.com>
2022-12-20 00:46:43 +0200

    Edited Main method

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Colors[][] imageArray = new Colors[120][120];
+
         new Drawer().randomFill(imageArray);
+        new Drawer2DShape().createImage(imageArray, "random");
 
         new Drawer2DShape().drawSquare(imageArray, 55);
-        new Drawer2DShape().createImage(imageArray, "output");
+        new Drawer2DShape().createImage(imageArray, "square");
     }
 }
